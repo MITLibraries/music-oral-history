7934c4a29ae2980fa949075554c39eadee52e58c
Zach Green <zgreen@mit.edu>
2014-03-24 15:58:46 -0400

    Remove div#information styles

--- single-interviews.php
+++ single-interviews.php
@@ -86,7 +86,7 @@ get_header();
 			
 		</div><!-- end div#titlebar -->
 
-		<div id="information" class="information group">
+		<div class="information group">
 	
 			<!-- Names &amp; Topics in Interview -->
 			
