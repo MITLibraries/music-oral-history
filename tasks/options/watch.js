a111e135ffdddc3691368babf73520bf1c033631
Zach Green <zgreen@mit.edu>
2014-03-27 12:46:54 -0400

    Grunt watch CSS sub-directories

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   css: {
-    files: ['css/*.scss'],
+    files: ['css/*.scss', 'css/**/*.css'],
     tasks: ['sass', 'autoprefixer', 'cssmin'],
     options: {
       spawn: false,
