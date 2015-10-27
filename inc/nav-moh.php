8f526b0f31e20c324a05dd59fcce834ef931f97b
pbruk <phillip.bruk@gmail.com>
2015-10-27 16:02:51 -0400

    add moh specific nav to local child directory

---
+++ inc/nav-moh.php
@@ -0,0 +1,27 @@
+<div class="menu--toggle"><!-- Mobile Hamburger icon -->
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="18.909px" height="13.091px" viewBox="2.182 6.545 18.909 13.091" enable-background="new 2.182 6.545 18.909 13.091" xml:space="preserve"><path d="M2.909,6.545h17.454c0.197,0,0.367,0.072,0.512,0.216c0.145,0.144,0.216,0.314,0.216,0.511s-0.071,0.367-0.216,0.511
+  c-0.145,0.144-0.314,0.216-0.512,0.216H2.909c-0.197,0-0.367-0.072-0.511-0.216C2.254,7.639,2.182,7.469,2.182,7.272
+  s0.072-0.367,0.216-0.511C2.542,6.617,2.712,6.545,2.909,6.545z M20.363,13.818H2.909c-0.197,0-0.367-0.072-0.511-0.216
+  s-0.216-0.314-0.216-0.511c0-0.196,0.072-0.367,0.216-0.511s0.314-0.216,0.511-0.216h17.454c0.197,0,0.367,0.072,0.512,0.216
+  s0.216,0.314,0.216,0.511c0,0.197-0.071,0.367-0.216,0.511S20.561,13.818,20.363,13.818z M20.363,19.636H2.909
+  c-0.197,0-0.367-0.071-0.511-0.216s-0.216-0.314-0.216-0.511s0.072-0.367,0.216-0.511c0.144-0.145,0.314-0.217,0.511-0.217h17.454
+  c0.197,0,0.367,0.072,0.512,0.217c0.145,0.144,0.216,0.314,0.216,0.511s-0.071,0.366-0.216,0.511S20.561,19.636,20.363,19.636z"/>
+  </svg>
+</div><!-- end hamburger icon -->
+<nav id="nav-main" class="nav-main flex-container nav-moh">
+  <div class="link-primary flex-container flex-end">
+    <a href="/music-oral-history" class="no-underline">Music Oral History home</a>     
+  </div><!-- end div.links-primary -->
+    <div class="link-primary flex-container flex-end">
+    <a href="/music-oral-history/interviewees" class="no-underline">Index of interviewees</a>
+  </div><!-- end div.links-primary -->
+  <div class="link-primary flex-container flex-end">
+    <a href="/music-oral-history/search-all-interviews" class="no-underline">Search all interviews</a>
+  </div><!-- end div.links-primary -->
+  <div class="link-primary flex-container flex-end">
+    <a href="/music-oral-history/about-the-project" class="no-underline">About the project</a>
+  </div>
+  <div class="link-primary flex-container flex-end">
+    <a href="/music-oral-history/terms-of-use" class="no-underline">Terms of use</a>
+  </div><!-- end div.links-primary -->
+</nav>
