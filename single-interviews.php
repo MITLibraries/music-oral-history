f98807faa19d095206b209320f163dea1116b861
Frances Botsford <frances@switchingprotocols.com>
2016-05-17 12:36:34 -0400

    adjust single interview video to use youtube

--- single-interviews.php
+++ single-interviews.php
@@ -164,142 +164,15 @@ get_header( 'moh' );
 		</div><!-- end div#information -->
 
 		<div class="content-page group">
-			<div id="playerWrap group">
-				<div id="video" class="video"></div>
-				<div id="transcript" class="transcript"></div>
-			</div>
-			<script>
-				var TECHTVID = "<?php echo types_render_field( 'video', null ); ?>";
-				var rpc = new easyXDM.Rpc({
-				    remote: "http://techtv.mit.edu/embeds/"+TECHTVID,
-				    container: "video",
-				    props: { style: { width: 442, height: 200 } }
-				  },
-	  			{
-				    local: {
-				      play: function(successFn, errorFn) {},
-				      pause: function(successFn, errorFn) {},
-				      stop: function(successFn, errorFn) {},
-				      resume: function(successFn, errorFn) {},
-				      toggle: function(successFn, errorFn) {},
-				      seek: function(seconds, successFn, errorFn) {},
-				      isPlaying: function(successFn, errorFn) {},
-				      isPaused: function(successFn, errorFn) {},
-				      playState: function(successFn, errorFn) {},
-				      position: function(successFn, errorFn) {},
-				      duration: function(successFn, errorFn) {},
-				      videoId: function(md5, successFn, errorFn) {},
-				      playFile: function(id, successFn, errorFn) {}
-				    },
-				    remote: {
-				      play: {},
-				      pause: {},
-				      stop: {},
-				      resume: {},
-				      toggle: {},
-				      seek: {},
-				      isPlaying: {},
-				      isPaused: {},
-				      playState: {},
-				      position: {},
-				      duration: {},
-				      videoId: {},
-				      playFile: {}
-				    }
-				  }
-				);
+		<div id="playerWrap" class="wrap-videoplayer">
 
-				window.p3_async_init = function(){
+			<script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
+			<iframe frameborder="0" height="300" id="myytplayer" src="https://www.youtube.com/embed/<?php echo types_render_field( 'youtube-video-id', null ); ?>?enablejsapi=1" type="text/html" width="440"></iframe><script type="text/javascript" src="https://static.3playmedia.com/p/projects/10129/files/<?php echo types_render_field( '3play-transcript-id', null ); ?>/plugins/10862.js"></script>
 
-				P3.PlayerInterface.techtv = function(id, instance) {
-	        var parent = this;
-	        parent.instance = instance;
+		</div>
 
-	        this.playState = "paused";
-	        this.playPosition = false;
-	        this.playDuration = false;
-	        this.playVideoId = TECHTVID;
 
-	        if (typeof rpc != "undefined" && rpc.play){
-	          this.play = function(){rpc.play();};
-	          this.pause = function(){rpc.pause();};
-	          this.play_state = function(){return parent.playState};
-	          this.position = function(){return parent.playPosition};
-	          this.video_id = function(){return parent.playVideoId};
-	          this.duration = function(){return parent.playDuration};
-	          this.seek = function(m){
-	            rpc.play();
-	            setTimeout(function(){
-	              var s = m / 1000;
-	              rpc.seek(s);
-	            },0);
-	          };
-	          /***
-
-	            TASK: THERE NEEDS TO BE SOME LOGIC HERE THAT ENABLES A NEW FILE AND TIMESTAMP
-	            TO BE LOADED INTO THE PLAYER, AND FOR PLAYBACK TO BEGIN AUTOMATICALLY AND FROM THAT TIMESTAMP
-	            RIGHT NOW IT IS TOO CLUNKY
-
-	          ***/
-	          this.play_file = function(obj){
-	            m = obj.m || 0;
-	            if (obj.video_id == parent.playVideoId){
-	              parent.seek(m);
-	            } else {
-	              rpc.playFile(obj.video_id, m/1000);
-	            }
-	          };
-	          /***
-
-	            TASK: THE UPDATE INTERVALS NEED TO BE OPTIMIZED, TRADING OFF USER EXPERIENCE AND PERFORMANCE
-
-	          ***/
-	          var SHORT_STAT_INTERVAL = 333;
-	          var LONG_STAT_INTERVAL = 3000;
-	          this.stat_interval = setInterval(function(){
-	            rpc.position(null,function(data){P3.get(0).player.interface.playPosition = data * 1000;});
-	          },SHORT_STAT_INTERVAL);
-	          this.long_stat_interval = setInterval(function(){
-	            rpc.isPlaying(null,function(data){P3.get(0).player.interface.playState = (data ? "PLAYING" : "PAUSED")});
-	            rpc.duration(null,function(data){P3.get(0).player.interface.playDuration = data * 1000;})
-	            rpc.videoId(null,function(data){P3.get(0).player.interface.playVideoId = data});
-	          },LONG_STAT_INTERVAL);
-	        }
-	      }
-
-				P3.init({
-				"video" : {
-				  player_type: "techtv",
-				  file_id: TECHTVID,
-				  platform_integration: true,
-				  transcript: {
-				    target: "transcript",
-				    skin: "minimal mitlib",
-				    // template: "bottom_search",
-				    width: 423,
-				    height: 214,
-				    can_print: true,
-				    can_download: true,
-				    can_collapse: true
-				  }
-				}
-
-				},"h_RJzPyn1vwbLhFaDLRxJUoNvT_756xr");
-				}
-
-				$(document).ready(function(){
-
-				$("#transcriptskin").change(function(){
-				P3.get(0).transcript.apply_skin($(this).val());
-				});
-				$("#playlistskin").change(function(){
-				P3.get(0).playlist.apply_skin($(this).val());
-				});
-
-				});	
-			</script>
-
-			<div id="interviewContents">
+			<div id="interviewContents" class="wrap-interviewcontents">
 				<?php
 
 				$contents_id = types_render_field( 'contents_id', null );
