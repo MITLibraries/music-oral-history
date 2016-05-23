69d65962ecad1d38048ddc1940e95150fa8a4ca6
Matt Bernhardt <mjbernha@mit.edu>
2016-05-23 08:47:07 -0400

    Cleans functions.php of PEAR.Functions.FunctionCallSignature

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * @since 1.1.11
  */
 
-add_filter('body_class', 'moh_body_class');
+add_filter( 'body_class', 'moh_body_class' );
 function moh_body_class($classes) {
 	$classes[] = 'moh';
 	return $classes;
@@ -19,32 +19,32 @@ function moh_scripts_styles() {
 
 	/* Register JS & CSS */
 
-	wp_register_style('global.css', get_stylesheet_directory_uri().'/css/build/minified/global.css', array('libraries-global'), '20140423');
+	wp_register_style( 'global.css', get_stylesheet_directory_uri().'/css/build/minified/global.css', array('libraries-global'), '20140423' );
 
-	wp_register_script('easyXDM', get_stylesheet_directory_uri().'/js/libs/easyXDM.min.js', array( 'jquery' ), false, false);
+	wp_register_script( 'easyXDM', get_stylesheet_directory_uri().'/js/libs/easyXDM.min.js', array( 'jquery' ), false, false );
 
-	wp_register_script('3play', get_stylesheet_directory_uri().'/js/libs/3playmedia.js', array( 'jquery' ), '3.0', true, true);
-	wp_register_script('3play-player', get_stylesheet_directory_uri().'/js/libs/3play.player.js', array( 'jquery' ), false, true);
-	wp_register_script('3play-hack', get_stylesheet_directory_uri().'/js/3play.hack.js', array('jquery', '3play', '3play-player' ), false, true);
+	wp_register_script( '3play', get_stylesheet_directory_uri().'/js/libs/3playmedia.js', array( 'jquery' ), '3.0', true, true );
+	wp_register_script( '3play-player', get_stylesheet_directory_uri().'/js/libs/3play.player.js', array( 'jquery' ), false, true );
+	wp_register_script( '3play-hack', get_stylesheet_directory_uri().'/js/3play.hack.js', array('jquery', '3play', '3play-player' ), false, true );
 
 	/* Queue scripts and styles */
 
-	wp_enqueue_style('global.css');
-	wp_enqueue_style('twentytwelve-style', get_stylesheet_uri(), array(), '20140328');
+	wp_enqueue_style( 'global.css' );
+	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20140328' );
 
 	/* Page-specific JS */
 
-	if (is_page('search-all-interviews')) {
-			wp_enqueue_style('p3');
-			wp_enqueue_script('easyXDM');
-			wp_enqueue_script('3play');
-			wp_enqueue_script('3play-player');
-			wp_enqueue_script('3play-hack');
+	if (is_page( 'search-all-interviews' )) {
+			wp_enqueue_style( 'p3' );
+			wp_enqueue_script( 'easyXDM' );
+			wp_enqueue_script( '3play' );
+			wp_enqueue_script( '3play-player' );
+			wp_enqueue_script( '3play-hack' );
 	}
 
 	if (is_single()) {
-		wp_enqueue_script('easyXDM');
-		wp_enqueue_script('3play');
+		wp_enqueue_script( 'easyXDM' );
+		wp_enqueue_script( '3play' );
 	}
 
 }
@@ -96,8 +96,8 @@ if ( !is_admin() ) {
 
 function sortInterviews($a, $b) {
 	
-	$aval = get_the_time('Ymd', $a->term_id);
-	$bval = get_the_time('Ymd', $b->term_id);
+	$aval = get_the_time( 'Ymd', $a->term_id );
+	$bval = get_the_time( 'Ymd', $b->term_id );
 	
 	
 	if ($aval == $bval) {
