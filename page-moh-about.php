f599f473a82c3836d916385c6a10b09f4f120344
Matt Bernhardt <mjbernha@mit.edu>
2016-05-24 09:21:43 -0400

    Cleans page-moh-about.php of PEAR.Functions.FunctionCallSignature

--- page-moh-about.php
+++ page-moh-about.php
@@ -3,17 +3,17 @@
 /* Template Name: MOH About */
 
 
-$pageRoot = getRoot($post);
-$section = get_post($pageRoot);
+$pageRoot = getRoot( $post );
+$section = get_post( $pageRoot );
 $isRoot = $section->ID == $post->ID;
 
 
-get_header('moh');
+get_header( 'moh' );
 
 
 ?>
 
-<?php get_template_part('inc/breadcrumbs'); ?>
+<?php get_template_part( 'inc/breadcrumbs' ); ?>
 
 		
 <?php while ( have_posts() ) : the_post(); ?>
@@ -24,7 +24,7 @@ get_header('moh');
 				<?php if ($isRoot): ?>
 				<h2><?php echo $section->post_title; ?></h2>
 				<?php else: ?>
-				<h2><a href="<?php echo get_permalink($section->ID) ?>"><?php echo $section->post_title; ?></a></h2>
+				<h2><a href="<?php echo get_permalink( $section->ID ) ?>"><?php echo $section->post_title; ?></a></h2>
 				<?php endif; ?>
 
 			</div>
