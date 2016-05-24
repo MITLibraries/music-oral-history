2c672887b1080c776de209641fbddebdfdd6b1cf
Matt Bernhardt <mjbernha@mit.edu>
2016-05-24 09:11:06 -0400

    Finishes easy cleaning of header-moh.php

--- header-moh.php
+++ header-moh.php
@@ -8,6 +8,7 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+
 ?><!DOCTYPE html>
 <!--[if lte IE 9]><html class="no-js lte-ie9" lang="en"><![endif]-->
 <!--[if !(IE 8) | !(IE 9) ]><!-->
@@ -24,11 +25,15 @@
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 
-<?php wp_head(); ?>
 <?php
-		//$askUrl = get_post_meta($post->ID, "ask_us_override", 1);
-		$askUrl = '';
-		if ($askUrl == '') $askUrl = '/ask';
+
+wp_head();
+
+// $askUrl = get_post_meta($post->ID, "ask_us_override", 1);
+$askUrl = '';
+if ( '' == $askUrl ) {
+	$askUrl = '/ask';
+}
 ?>
 	<script>
 		todayDate="";
@@ -47,14 +52,11 @@
 			
 			<a class="link-logo-mit" href="http://www.mit.edu" alt="Massaschusetts Institute of Technology logo"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="54" height="28" viewBox="0 0 54 28" enable-background="new 0 0 54 28" xml:space="preserve" class="logo-mit"><rect x="28.9" y="8.9" width="5.8" height="19.1" class="color"/><rect width="5.8" height="28"/><rect x="9.6" width="5.8" height="18.8"/><rect x="19.3" width="5.8" height="28"/><rect x="38.5" y="8.9" width="5.8" height="19.1"/><rect x="38.8" width="15.2" height="5.6"/><rect x="28.9" width="5.8" height="5.6"/></svg></a>
 
-	
-
 		</header>
 
+<?php
 
-		<?php 
+$pageRoot = getRoot( $post );
+$section = get_post( $pageRoot );
 
-			$pageRoot = getRoot( $post );
-			$section = get_post( $pageRoot );
-
-		?>
+?>
